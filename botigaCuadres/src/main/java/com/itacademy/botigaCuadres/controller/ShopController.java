ac557666dcf95f4c1f3554ae073acef5b3e9b8f3
Anton <hagane.link0@gmail.com>
2020-10-01 12:28:08 +0200

    Created ShopController

--- botigaCuadres/src/main/java/com/itacademy/botigaCuadres/controller/ShopController.java
+++ botigaCuadres/src/main/java/com/itacademy/botigaCuadres/controller/ShopController.java
@@ -3,8 +3,11 @@ package com.itacademy.botigaCuadres.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itacademy.botigaCuadres.dto.ShopResponseDto;
@@ -25,6 +28,17 @@ public class ShopController {
 	public ResponseEntity<Iterable<ShopResponseDto>> viewAllShops() {
 		return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
 		
+	}
+	
+	@PutMapping("/shops/")
+	public void insertShop(ShopResponseDto shop) {
+		service.insertShop(shop);
+	}
+	
+	@DeleteMapping("/shops/{name}")
+	public void deleteShop(@RequestParam String name) {
+		ShopResponseDto tempShop = service.getShop(name);
+		service.deleteShop(tempShop);
 	}
 	
 
